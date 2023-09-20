e806f2ec0f2852ea49d9216d0ca4167e3da97662
Marco Viana <marcoviana100@gmail.com>
2023-09-20 01:15:45 -0300

    feat: classe JsonImporter que herda de Importer implementada

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,3 +1,5 @@
+# Importa o módulo json para lidar com arquivos JSON.
+import json
 from typing import Dict, Type, List
 from abc import ABC, abstractmethod
 from inventory_report.product import Product
@@ -13,8 +15,32 @@ class Importer(ABC):
         pass
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    # Implementa o método import_data exigido pela classe abstrata Importer.
+    def import_data(self) -> List[Product]:
+        # Abre o arquivo JSON no modo de leitura ("r").
+        with open(self.path, "r") as json_file:
+            # Lê os dados do arquivo JSON e os converte em um objeto Python.
+            data = json.load(json_file)
+
+        products = []
+
+        # Itera sobre os itens do arquivo JSON.
+        for item in data:
+            # Cria um objeto Product para cada item,
+            # com os atributos correspondentes.
+            product = Product(
+                id=item["id"],
+                product_name=item["product_name"],
+                company_name=item["company_name"],
+                manufacturing_date=item["manufacturing_date"],
+                expiration_date=item["expiration_date"],
+                serial_number=item["serial_number"],
+                storage_instructions=item["storage_instructions"]
+            )
+            products.append(product)
+
+        return products
 
 
 class CsvImporter:
