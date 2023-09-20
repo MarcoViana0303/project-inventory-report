9e27f094b2030cbba299c0caac5d41b6eeea70a9
Marco Viana <marcoviana100@gmail.com>
2023-09-20 02:33:13 -0300

    feat: implementado a classe CvsImporter, com objetivo de ler arquivos csv

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,3 +1,4 @@
+import csv
 # Importa o módulo json para lidar com arquivos JSON.
 import json
 from typing import Dict, Type, List
@@ -43,8 +44,30 @@ class JsonImporter(Importer):
         return products
 
 
-class CsvImporter:
-    pass
+class CsvImporter(Importer):
+    def import_data(self) -> List[Product]:
+        products = []
+
+        # Abre o arquivo CSV no modo de leitura.
+        with open(self.path, "r") as csv_file:
+            # Cria um leitor de CSV baseado em dicionário.
+            csv_reader = csv.DictReader(csv_file)
+
+            for row in csv_reader:
+                # Cria um objeto Produto com os dados de cada linha do CSV.
+                product = Product(
+                    id=row["id"],
+                    product_name=row["product_name"],
+                    company_name=row["company_name"],
+                    manufacturing_date=row["manufacturing_date"],
+                    expiration_date=row["expiration_date"],
+                    serial_number=row["serial_number"],
+                    storage_instructions=row["storage_instructions"],
+                )
+
+                products.append(product)
+
+        return products
 
 
 # Não altere a variável abaixo
